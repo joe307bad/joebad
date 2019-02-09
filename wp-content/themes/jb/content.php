44d639daa82b19283ed3df50786a056e7e7813e3
Joe <badaczewskij@live.com>
2019-02-09 16:16:13 -0500

    updating resuem and homepage

--- wp-content/themes/jb/content.php
+++ wp-content/themes/jb/content.php
@@ -1,7 +1,7 @@
 <div id="content">
     <img height="200" src="/wp-content/themes/jb/images/logo.png" />
     <h1>Joe Badaczewski</h1>
-    <h2>Software Engineer at <a target="_blank" href="https://www.psionline.com">PSI</a></h2>
+    <h2>Software Engineer at <a target="_blank" href="https://idelic.com/">Idelic</a></h2>
     <ul>
         <li>
             <a title='LinkedIn' href="https://www.linkedin.com/in/joe-badaczewski-273747b">
@@ -16,11 +16,6 @@
         <li>
             <a title="Github" href="https://github.com/joe307bad">
                 <i class="fa fa-github" aria-hidden="true"></i>
-            </a>
-        </li>
-        <li>
-            <a title="Resumé" href="/wp-content/Badaczewski_Resumé.pdf">
-                <i class="fa fa-file-text-o" aria-hidden="true"></i>
             </a>
         </li>
     </ul>
