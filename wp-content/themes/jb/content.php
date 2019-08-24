be9d62c2ddb0bd57df639b35d32d05c20011336f
Joe Badaczewski <joe307bad@gmail.com>
2019-08-24 15:15:54 -0400

    updating job title

--- wp-content/themes/jb/content.php
+++ wp-content/themes/jb/content.php
@@ -1,7 +1,7 @@
 <div id="content">
     <img height="200" src="/wp-content/themes/jb/images/logo.png" />
     <h1>Joe Badaczewski</h1>
-    <h2>Software Engineer at <a target="_blank" href="https://idelic.com/">Idelic</a></h2>
+    <h2>Senior Software Engineer</h2>
     <ul>
         <li>
             <a title='LinkedIn' href="https://www.linkedin.com/in/joe-badaczewski-273747b">
